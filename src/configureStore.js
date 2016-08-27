58b6de4abd35b1f06a66ef7fe4280fff8f340085
Ivan Kotov <kotovivan@gmail.com>
2016-08-27 15:21:03 +0300

    feat(router): Router in root node

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,24 +1,24 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { routerMiddleware, routerReducer } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 import appReducer from './reducers/app-reducer';
+
+const reducers = {
+    routing: routerReducer,
+    app: appReducer
+};
 
 import thunk from 'redux-thunk';
 
-import historyPlugin from './plugins/history';
-
 const MIDDLEWARES = [
-    thunk
-];
-
-const PLUGINS = [
-    historyPlugin
+    routerMiddleware(browserHistory), thunk
 ];
 
 function configureStore(isHotLoaderRequired = false) {
     const middleware = applyMiddleware(...MIDDLEWARES);
 
-    return state => {
-        const store = createStore(appReducer, state, middleware);
-        PLUGINS.forEach(plugin => plugin(store));
+    return (state = {}) => {
+        const store = createStore(combineReducers(reducers), state, middleware);
 
         if (isHotLoaderRequired && module.hot) {
             module.hot.accept('./reducers/app-reducer', () => {
