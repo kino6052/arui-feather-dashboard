f451e9d3d0277d27149a3a711c95be205926fbde
Ivan Lyagushkin <ivliag@gmail.com>
2017-01-20 16:38:24 +0300

    feat(docker-build): prepare tmp-package.json

---
+++ prepare-build-package.js
@@ -0,0 +1,34 @@
+'use strict';
+/**
+ *  Создает файл tmp-package.json,
+ *  который используется для установки зависимостей в build-контейнере.
+ *
+ *  1. Фиксирует версию проекта, чтобы package.json
+ *     разных сборок совпадал и докер использовал кэш.
+ *  2. Вырезает из package.json все зависимости для тестирования,
+ *     они не нужны для сборки, PhantomJS ставится долго.
+ */
+
+const fs = require('fs');
+let packages = require('./package.json');
+
+function isTestDependency(dependency) {
+    return !!dependency.match(/chai|karma|lint|istanbul|mocha/ig);
+}
+
+function removeTestDependensies(deps) {
+    let result = {};
+    for (let key in deps) {
+        if (!isTestDependency(key)) {
+            result[key] = deps[key];
+        }
+    }
+
+    return result;
+}
+
+packages.version = '42.0.0';
+packages.dependencies = removeTestDependensies(packages.dependencies);
+packages.devDependencies = removeTestDependensies(packages.devDependencies);
+
+fs.writeFileSync('./tmp-package.json', JSON.stringify(packages, null, 4));
