0f4310351b154e854a5e04a55c7bfce014f06e16
akitov <akitov@alfabank.ru>
2017-01-26 20:42:43 +0300

    fix(*): fix default build process, use contextRoot in router

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM docker.moscow.alfaintra.net/mhart/alpine-node:6.9.4
 MAINTAINER alfabank
 
-ADD ssh /root/.ssh/
 WORKDIR /src
 ADD . /src
--- src/root.jsx
+++ src/root.jsx
@@ -1,14 +1,18 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
 import routes from './routes';
+import { createHistory } from 'history';
 
 const propTypes = {
     store: PropTypes.object.isRequired,
     history: PropTypes.object
 };
 
-const Root = ({ store, history }) => {
+const Root = ({ store }) => {
+    const history = useRouterHistory(createHistory)({
+        basename: store.getState().settings.contextRoot
+    });
     return (
         <Provider store={ store }>
             <Router key={ Date.now() } history={ history } routes={ routes } />
