82b39f5230b49c70d37e42a7a03c2e1c1f87e766
akitov <akitov@alfabank.ru>
2017-03-28 09:35:01 +0300

    fix(router): fix server router redirects handling

--- src/root.jsx
+++ src/root.jsx
@@ -1,18 +1,14 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import { Router, useRouterHistory } from 'react-router';
+import { Router } from 'react-router';
 import routes from './routes';
-import { createHistory } from 'history';
 
 const propTypes = {
     store: PropTypes.object.isRequired,
     history: PropTypes.object
 };
 
-const Root = ({ store }) => {
-    const history = useRouterHistory(createHistory)({
-        basename: store.getState().settings.contextRoot
-    });
+const Root = ({ store, history }) => {
     return (
         <Provider store={ store }>
             <Router key={ Date.now() } history={ history } routes={ routes } />
