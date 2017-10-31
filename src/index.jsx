217c1e938d43a745b1efb92bcbf3711876668da1
Dmitry Samokhvalov <dsamokhvalov@alfabank.ru>
2017-10-31 14:39:02 +0300

    fix(history): fix history basename

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import createHistory from 'history/createBrowserHistory';
 
 import Root from './root';
-import configureStore, { history } from './configure-store';
+import configureStore from './configure-store';
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const HOT_LOADER = !!process.HOT_LOADER && !IS_PRODUCTION;
+const CAN_USE_DOM = !!(typeof window !== 'undefined' && window.document && window.document.createElement);
 
 let configureStoreLocal = configureStore(HOT_LOADER);
 
 if (typeof window !== 'undefined') {
     // eslint-disable-next-line no-underscore-dangle
     window.__main = (state) => {
-        let store = configureStoreLocal(state);
+        const history = CAN_USE_DOM
+            ? createHistory({
+                basename: (state.settings && state.settings.contextRoot) || ''
+            })
+            : null;
+
+        let store = configureStoreLocal(state, history);
 
         if (HOT_LOADER) {
             ReactDOM.render(
